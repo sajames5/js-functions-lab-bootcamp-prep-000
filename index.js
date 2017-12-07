50491c17a5c33a12fe2a663406894d3c1ab6333b
Stephanie <s.a.james5@gmail.com>
2017-12-07 18:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function happyHolidays(greeting) {return `Happy holidays!`}
 function happyHolidaysTo(you) {return `Happy holidays, you!`}
-function happyHolidayTo(Independence Day, you) {return `Happy Independence Day, you!`}
+function happyHolidayTo(IndependenceDay, you) {return `Happy Independence Day, you!`}
